2c8b9d73fd7501cb71a855c67500960e59dd420c
Mischa Dasberg <mischa@dasberg.nl>
2015-06-04 11:46:00 +0200

    Added watcher to remove answer when input has changed

--- src/js/grandfather.js
+++ src/js/grandfather.js
@@ -1,13 +1,22 @@
 'use strict';
 
-function GrandfatherCtrl() {
-    this.question = {};
-    this.answer = '';
-
-    this.answerQuestion = function () {
+function GrandfatherCtrl($scope) {
+    var vm = this;
+    vm.question = {};
+    vm.answer = undefined;
+    
+    vm.answerQuestion = function () {
         this.answer = 'Chocolate!';
     }
+
+    $scope.$watch('gf.question.text', function(current, original) {
+        if(original !== current) {
+            vm.answer = undefined;
+        }
+    });
 }
+
+GrandfatherCtrl.$inject = ['$scope'];
 
 angular.module('grandfatherOfAllKnowledge', [])
     .controller('GrandfatherCtrl', GrandfatherCtrl);
